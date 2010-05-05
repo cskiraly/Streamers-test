1750900537fb808d028a2d497f5dc718d4bf162e
Csaba Kiraly <kiraly@disi.unitn.it>
2010-05-05 12:04:23 +0200

    added update script

---
+++ update.sh
@@ -0,0 +1,23 @@
+cd Streamers
+cd NAPA
+git pull -f
+mkdir -p m4 config
+autoreconf --force -I config -I m4 --install
+./configure --with-libevent2=`pwd`/../Event  --with-libconfuse=`pwd`/../Conf
+echo "//blah" > common/chunk.c
+make -C common
+make -C dclog
+make -C rep
+make -C monl
+make -C ml 
+cd ..
+cd GRAPES
+git pull -f
+make -C som
+cd ..
+git pull -f
+ML=1 MONL=1 make clean
+ML=1 MONL=1 STATIC=1 make clean
+LIBEVENT=`pwd`/Event ML=1 MONL=1 make
+LIBEVENT=`pwd`/Event ML=1 MONL=1 STATIC=1 make
+
