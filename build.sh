0503d2778185347098ebb1ce4cc5a8e724e5281c
Csaba Kiraly <kiraly@disi.unitn.it>
2010-06-16 09:06:14 +0200

    speed up make build process for the impatient
    added some parallelism to the build process. Should not cause
    any problems with correctly written Makefiles

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@ which libtoolize || { echo "please install libtool!"; exit 1; }
 which autoconf || { echo "please install autoconf!"; exit 1; }
 which autopoint || { echo "please install autopoint!"; exit 1; }
 
+MAKE="make -j 4"
+
 cd libevent-2.0.3-alpha
 ./configure --prefix `pwd`/../Streamers/Event
 #./configure --prefix `pwd`/../Streamers/Event --disable-shared
-make; make install
+$MAKE; $MAKE install
 cd ..
 
 cd confuse
 ./autogen.sh --prefix=`pwd`/../Streamers/Conf
-make; make install
+$MAKE; $MAKE install
 cd ..
 
 cd Streamers
@@ -21,17 +23,17 @@ mkdir -p m4 config
 autoreconf --force -I config -I m4 --install
 ./configure --with-libevent2=`pwd`/../Event  --with-libconfuse=`pwd`/../Conf
 echo "//blah" > common/chunk.c
-make -C common
-make -C dclog
-make -C rep
-make -C monl
-make -C ml 
+$MAKE -C common
+$MAKE -C dclog
+$MAKE -C rep
+$MAKE -C monl
+$MAKE -C ml 
 cd ..
-make -C GRAPES/som
-make -C ffmpeg
-make clean
-LIBEVENT=`pwd`/Event ML=1  make
-LIBEVENT=`pwd`/Event ML=1 STATIC=1 make
-make clean
-LIBEVENT=`pwd`/Event ML=1 MONL=1 make
-LIBEVENT=`pwd`/Event ML=1 MONL=1 STATIC=1 make
+$MAKE -C GRAPES/som
+$MAKE -C ffmpeg
+$MAKE clean
+LIBEVENT=`pwd`/Event ML=1  $MAKE
+LIBEVENT=`pwd`/Event ML=1 STATIC=1 $MAKE
+$MAKE clean
+LIBEVENT=`pwd`/Event ML=1 MONL=1 $MAKE
+LIBEVENT=`pwd`/Event ML=1 MONL=1 STATIC=1 $MAKE
