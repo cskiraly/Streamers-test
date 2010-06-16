ddf8b8f2e718a34373c35144eb6fb360781bc234
Csaba Kiraly <kiraly@disi.unitn.it>
2010-06-16 08:59:54 +0200

    fix the version of libconfuse to 2.7
    newer snapshots created problems

--- download.sh
+++ download.sh
@@ -1,10 +1,14 @@
 git clone http://www.disi.unitn.it/~kiraly/SharedGits/Streamers.git
 git clone http://www.disi.unitn.it/~kiraly/PublicGits/GRAPES.git
+
 wget http://www.monkey.org/~provos/libevent-2.0.3-alpha.tar.gz
 tar xvzf libevent-2.0.3-alpha.tar.gz
+
+#get libconfuse 2.7 (warning: newer version were not tested)
 #wget http://savannah.nongnu.org/download/confuse/confuse-2.7.tar.gz
 #tar xvzf confuse-2.7.tar.gz
 git clone git://git.sv.gnu.org/confuse.git
+cd confuse; git checkout V2_7; cd ..
 
 cd Streamers
 git checkout -b for-demo origin/for-demo
