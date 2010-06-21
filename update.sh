b84bfcaad16d22936ef4ab0bb062c79a9fb388f6
Csaba Kiraly <kiraly@disi.unitn.it>
2010-06-21 21:59:05 +0200

    update update.sh to download from svn

--- update.sh
+++ update.sh
@@ -1,11 +1,8 @@
-cd Streamers
+MAKE="make -j 4"
+
+cd OfferStreamer
 cd NAPA
-git fetch
-git reset --hard origin/for-demo-NAPA
-mkdir -p m4 config
-autoreconf --force -I config -I m4 --install
-./configure --with-libevent2=`pwd`/../Event  --with-libconfuse=`pwd`/../Conf
-echo "//blah" > common/chunk.c
+svn up
 make -C common
 make -C dclog
 make -C rep
@@ -13,20 +10,10 @@ make -C monl
 make -C ml 
 cd ..
 
-cd GRAPES
-git fetch
-git reset --hard origin/for-demo-GRAPES
-make -C som clean
-make -C som
-cd ..
-
-git fetch
-git reset --hard origin/for-demo
-ML=1 make clean
-ML=1 STATIC=1 make clean
-LIBEVENT=`pwd`/Event ML=1 make
-LIBEVENT=`pwd`/Event ML=1 STATIC=1 make
-ML=1 MONL=1 make clean
-ML=1 MONL=1 STATIC=1 make clean
-LIBEVENT=`pwd`/Event ML=1 MONL=1 make
-LIBEVENT=`pwd`/Event ML=1 MONL=1 STATIC=1 make
+svn up
+$MAKE clean
+LIBEVENT=`pwd`/../Event ML=1  $MAKE || exit
+LIBEVENT=`pwd`/../Event ML=1 STATIC=1 $MAKE || exit
+$MAKE clean
+LIBEVENT=`pwd`/../Event ML=1 MONL=1 $MAKE || exit
+LIBEVENT=`pwd`/../Event ML=1 MONL=1 STATIC=1 $MAKE || exit
