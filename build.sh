28ad3f3c4db86f9cd9bd2599315161bdefd7d05a
Csaba Kiraly <kiraly@disi.unitn.it>
2010-06-21 18:37:41 +0200

    update build process

--- build.sh
+++ build.sh
@@ -7,33 +7,35 @@ which autopoint || { echo "please install gettext (or autopoint) !"; exit 1; }
 MAKE="make -j 4"
 
 cd libevent-2.0.3-alpha
-./configure --prefix `pwd`/../Streamers/Event
-#./configure --prefix `pwd`/../Streamers/Event --disable-shared
+./configure --prefix `pwd`/../Event
 $MAKE; $MAKE install
 cd ..
 
-cd confuse
-./autogen.sh --prefix=`pwd`/../Streamers/Conf
+cd confuse-2.7
+./configure --prefix=`pwd`/../Conf
 $MAKE; $MAKE install
 cd ..
 
-cd Streamers
+cd OfferStreamer
 cd NAPA
 mkdir -p m4 config
 autoreconf --force -I config -I m4 --install
-./configure --with-libevent2=`pwd`/../Event  --with-libconfuse=`pwd`/../Conf
-echo "//blah" > common/chunk.c
+./configure --with-libevent2=`pwd`/../../Event  --with-libconfuse=`pwd`/../../Conf
 $MAKE -C common
 $MAKE -C dclog
 $MAKE -C rep
 $MAKE -C monl
-$MAKE -C ml 
+cd ml
+./autogen.sh
+./configure --with-libevent2=`pwd`/../../../Event
+$MAKE
 cd ..
-$MAKE -C GRAPES/som
+cd ..
+$MAKE -C GRAPES
 $MAKE -C ffmpeg
 $MAKE clean
-LIBEVENT=`pwd`/Event ML=1  $MAKE
-LIBEVENT=`pwd`/Event ML=1 STATIC=1 $MAKE
+LIBEVENT=`pwd`/../Event ML=1  $MAKE
+LIBEVENT=`pwd`/../Event ML=1 STATIC=1 $MAKE
 $MAKE clean
-LIBEVENT=`pwd`/Event ML=1 MONL=1 $MAKE
-LIBEVENT=`pwd`/Event ML=1 MONL=1 STATIC=1 $MAKE
+LIBEVENT=`pwd`/../Event ML=1 MONL=1 $MAKE
+LIBEVENT=`pwd`/../Event ML=1 MONL=1 STATIC=1 $MAKE
