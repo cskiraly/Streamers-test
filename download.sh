6de22c026a652fe5f9efad7d1438004cedb9a49d
Csaba Kiraly <kiraly@disi.unitn.it>
2010-06-21 17:37:28 +0200

    download from SVN

--- download.sh
+++ download.sh
@@ -1,20 +1,17 @@
-git clone http://www.disi.unitn.it/~kiraly/SharedGits/Streamers.git
-git clone http://www.disi.unitn.it/~kiraly/PublicGits/GRAPES.git
+svn co https://repository.napa-wine.eu/svn/napawine-software/trunk/Applications/OfferStreamer
+svn co https://repository.napa-wine.eu/svn/napawine-software/trunk/GRAPES
 
 # get libevent 2.0.3 (warning: newer version were not tested)
 wget http://www.monkey.org/~provos/libevent-2.0.3-alpha.tar.gz
 tar xvzf libevent-2.0.3-alpha.tar.gz
 
 #get libconfuse 2.7 (warning: newer version were not tested)
-#wget http://savannah.nongnu.org/download/confuse/confuse-2.7.tar.gz
-#tar xvzf confuse-2.7.tar.gz
-git clone git://git.sv.gnu.org/confuse.git
-cd confuse; git checkout V2_7; cd ..
+wget http://savannah.nongnu.org/download/confuse/confuse-2.7.tar.gz
+tar xvzf confuse-2.7.tar.gz
+#git clone git://git.sv.gnu.org/confuse.git
+#cd confuse; git checkout V2_7; cd ..
 
-cd Streamers
-git checkout -b for-demo origin/for-demo
-cp -r ../GRAPES NAPA
-cd NAPA; git checkout -b for-demo-NAPA origin/for-demo-NAPA; cd ..
-mv ../GRAPES .
-cd GRAPES; git checkout -b for-demo-GRAPES origin/for-demo-GRAPES; cd ..
+cd OfferStreamer
+mv ../GRAPES NAPA
+ln -s NAPA/som GRAPES
 make ffmpeg
