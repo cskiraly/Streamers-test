2b349fa2fd929a38d1ea7ffdfb974b68ba532ce2
Csaba Kiraly <kiraly@disi.unitn.it>
2011-01-26 23:22:14 +0100

    cycletest: use limitbw script

--- cycletest.sh
+++ cycletest.sh
@@ -12,48 +12,13 @@ usage()
 [ -e "$1" ] || { usage; exit 1; }
 . $1
 
-limitupdown_init()
-{
-  ${tc} qdisc del dev $IFDEV root 2>/dev/null
-  ${tc} qdisc add dev $IFDEV root handle 1: htb default 1
-  ${tc} class add dev $IFDEV parent 1: classid 1:1 htb rate 100mbit burst 15k
-}
-
-limitupdown()
-{
-  PORT=$1
-  DOWN_SPEED=$2 #mbit/s
-  DOWN_LOSS=$3 #%
-  DOWN_DELAY=$4 #msec
-  UP_SPEED=$5 #mbit/s
-  UP_LOSS=$6 #%
-  UP_DELAY=$7 #msec
-
-   # limit downlink
-  HANDLE=$PORT
-  ${tc} class add dev $IFDEV parent 1:1 classid 1:$HANDLE htb rate ${DOWN_SPEED}mbit burst 15k || exit
-  ${tc} qdisc add dev $IFDEV parent 1:$HANDLE handle $HANDLE: netem loss ${DOWN_LOSS}% delay ${DOWN_DELAY}msec || exit
-  ${tc} filter add dev $IFDEV protocol ip parent 1:0 prio 1 u32 \
-    match ip protocol $PROTOCOL 0xff \
-    match ip dport $PORT 0xffff \
-    flowid 1:$HANDLE || exit
-
-   # limit uplink
-  HANDLE=$(($PORT + 2000))
-  ${tc} class add dev $IFDEV parent 1:1 classid 1:$HANDLE htb rate ${UP_SPEED}mbit burst 15k || exit
-  ${tc} qdisc add dev $IFDEV parent 1:$HANDLE handle $HANDLE: netem loss ${UP_LOSS}% delay ${UP_DELAY}msec || exit
-  ${tc} filter add dev $IFDEV protocol ip parent 1:0 prio 1 u32 \
-    match ip protocol $PROTOCOL 0xff \
-    match ip sport $PORT 0xffff \
-    flowid 1:$HANDLE || exit
-}
-
 # Kill everything we've started on exit (with trap).
 bashkilltrap()
 {
   # ok that's the end
   # you can add here all possible postprocessing,
-#  sudo tc qdisc del dev lo root 2>/dev/null
+
+  $LIMITBW end $IFDEV
 
   ps -o pid= --ppid $$ | xargs kill 2>/dev/null
 }
@@ -92,23 +57,19 @@ for BIN in `eval echo $BINs`; do
 
   if [ ! -e $CSV ] ; then
 
-  limitupdown_init
+  $LIMITBW init $IFDEV
 
-  for PORT in `seq 6667 $((6667+$PEERNUM1-1))`; do
-    limitupdown $PORT $DOWNRATE 0 $DELAY $UPRATE1 0 $DELAY
-  done;
+  $LIMITBW peers $IFDEV 6667 $((6667+$PEERNUM1-1)) $DOWNRATE $UPRATE1 0 $DELAY
   $TESTSH -v $VIDEO -I lo -f abouttopub -p "--measure_start $MEASURE_START --measure_every $MEASURE_EVERY -c $CPS -b $CHBUF -o $REORDBUF -M $NEIGH -n stun_server=0" -s "-M 0 -n stun_server=0" -X 0 -e ${BINPREFIX}${BIN}${BINPOSTFIX} -N $PEERNUM1 >a &
   PIDS+=" $!"
 
-  for PORT in `seq $((6667+$PEERNUM1)) $((6667+$PEERNUM1+$PEERNUM2-1))`; do
-    limitupdown $PORT $DOWNRATE 0 $DELAY $UPRATE2 0 $DELAY
-  done;
+  $LIMITBW peers $IFDEV $((6667+$PEERNUM1)) $((6667+$PEERNUM1+$PEERNUM2-1)) $DOWNRATE $UPRATE2 0 $DELAY
   $TESTSH -I lo -f abouttopub -p "--measure_start $MEASURE_START --measure_every $MEASURE_EVERY -c $CPS -b $CHBUF -o $REORDBUF -M $NEIGH -n stun_server=0" -s "-M 0 -n stun_server=0" -X 0 -e ${BINPREFIX}${BIN}${BINPOSTFIX} -N $PEERNUM2 -P $((6667+$PEERNUM1)) -Z >a &
   PIDS+=" $!"
 
   sleep $DURATION
   kill $PIDS
-  sudo tc qdisc del dev lo root
+  $LIMITBW end $IFDEV
   sleep 30
 
   echo -e "#dummy,src,from,to,measure,value,stringval,channel,time,peergrp,$SCENARIO_HDR\n" >$CSV
