ddf7776dfad04d5c68a2d985a96290605e99f310
Csaba Kiraly <kiraly@disi.unitn.it>
2011-01-27 14:21:21 +0100

    add clearcolumn.pl script to clear a column of a csv

---
+++ clearcolumn.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+
+if ($#ARGV+1 != 1) {
+  printf(STDERR "Usage: mean.pl <column>\n");
+  exit 1;
+}
+
+my $meancolumn = $ARGV[0];
+
+my $colcount;
+my %sums = ();
+my %cnt = ();
+while( my $line = <STDIN> ){
+  chomp($line);
+  if ($line !~ /^\s*#/) {
+    my @fields = split(/\,/,$line);
+    if (scalar(@fields) < $meancolumn) { next;}
+
+    if (! defined($colcount)) {
+      $colcount = scalar(@fields);
+    } else {
+      if (scalar(@fields) != $colcount) {
+        printf(STDERR "Warning: skipping line with wrong column count: $line\n");
+        next;
+      }
+    }
+
+    my $prefix = join(',',@fields[0..$meancolumn-1]);
+    my $postfix = join(',',@fields[($meancolumn+1) .. (scalar(@fields)-1)]);
+
+    print "$prefix,,$postfix\n";
+
+  } else {
+    print "$line\n"; # to preserve the header
+  }
+}
+
